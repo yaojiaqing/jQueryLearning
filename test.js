ce24b68cde6c98d04795a3886530f9fbe31f05f4
yaojiaqing <yaojiaqing@hotmail.com>
2017-11-30 15:12:06 +0800

    commit

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-(function () {
+(function (window,undefined) {
+    var
+        readyList,
+        rootAct,
+        core_strundefined = typeof undefined,
+        location = window.location,
+        document = window.document,
+        docElem = window.documentElement,
+
+        _Act = window.Act,
+        __ = window._,
+
+        core_version = "0.1.0",
+
+        Act = function(selector,context) {
+            return new Act.fn.init(selector,context,rootAct);                
+        };
     console.log("auto running...");
 })();
